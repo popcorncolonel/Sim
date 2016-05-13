eed49b22b7a8d898598994b757d362bd1ee445e5
Eric Bailey <popcorncolonel@gmail.com>
2016-05-13 18:53:41 -0400

    more neuron work

--- brain.py
+++ brain.py
@@ -30,12 +30,33 @@ class Actuator(Neuron):
         self.sim[self.organism.x][self.organism.y].remove(self.organism)
         self.organism.x += self.delta_x
         self.organism.y += self.delta_y
+        self.organism.x = self.organism.x % self.sim.width
+        self.organism.y = self.organism.y % self.sim.height
         self.sim[self.organism.x][self.organism.y].add(self.organism)
 
+class AttackActuator(Neuron):
+    """
+    Can battle another organism if they're one unit away from you
+    """
+
+
+class MateActuator(Neuron):
+    """
+    Can mate with another organism if they're one unit away from you
+    """
 
 class Sensors(Neuron):
-    def __init__(self):
+    """
+    Can sense whether there is an organism immediately surrounding you,
+    can compare the power of organisms,
+    can look left,right,up,upright,downleft,...,
+    can see if you are the same representative character
+    TODO: add more sensors
+    """
+    def __init__(self, sim, org):
         super().__init__()
+        self.sim = sim
+        self.org = org
 
     def activate(self):
         for conn in self.outgoing_connections:
@@ -51,3 +72,21 @@ class NeuronConnection:
     def activate(self):
         self.end.activate()
 
+class Actuators:
+    def __init__(self, sim, org):
+        self.up = Actuator(sim, org, delta_x=0, delta_y=-1)
+        self.down = Actuator(sim, org, delta_x=0, delta_y=1)
+        self.right = Actuator(sim, org, delta_x=1, delta_y=0)
+        self.left = Actuator(sim, org, delta_x=-1, delta_y=0)
+        self.upright = Actuator(sim, org, delta_x=1, delta_y=-1)
+        self.upleft = Actuator(sim, org, delta_x=-1, delta_y=-1)
+        self.downright = Actuator(sim, org, delta_x=1, delta_y=1)
+        self.downleft = Actuator(sim, org, delta_x=-1, delta_y=1)
+        self.list = [self.up,
+                     self.down,
+                     self.right,
+                     self.left,
+                     self.upright,
+                     self.upleft,
+                     self.downright,
+                     self.downleft]
